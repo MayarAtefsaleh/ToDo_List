f02328176ce9e31254fa426b3647c6d86c08ba01
Mayar <Mayar@LAPTOP-DQN6LHIR.com>
2023-12-02 10:26:12 +0200

    new files added

---
+++ navbar.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-expand-lg bg-light pt-4 pb-4" style="background-color: #e3f2fd;">
+    <div class="container d-flex align-items-center justify-content-center">
+        <a class="navbar-brand" href="#" style="color: #E75480;">MY TO
+            DO LIST</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+    </div>
+</nav>
